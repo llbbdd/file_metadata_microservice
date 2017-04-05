6b8d24f02615112cf7ce161375791b110a99bafc
generalwellbeing <leighbrodie@gmail.com>
2017-04-05 14:52:37 +0000

    form, routes, accepting of file completed

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 var express = require('express');
+var multer  = require('multer');
+var fs = require('fs');
+
+var port = 8080;
+var uploadFolder = 'uploads/';
+
+var app = express();
+var upload = multer({dest: uploadFolder});
+
+app.get('/', function(req, res){
+    fs.readFile('form.html', function (err, data) {
+        if(err){
+            console.log(err);
+        }
+        
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        
+        res.write(data);
+        res.end();
+    });
+});
+
+app.post('/', upload.single('file'), function (req, res, next) {
+    res.send(JSON.parse('{"size":"' + req.file.size + '"}'));
+    res.end();
+})
+
+app.listen(port);
